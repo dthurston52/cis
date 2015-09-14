695e290fcd87d534211f32729fbb1c21bcb27778
U-NASHVILLE\dthurston <dthurston@dell821FXW1.Nashville.int>
2015-09-14 08:03:31 -0500

    Fix duplicate sshd_config file declaration c0068.pp

--- manifests/linuxcontrols/c0068.pp
+++ manifests/linuxcontrols/c0068.pp
@@ -4,11 +4,11 @@
 #
 
 class cis::linuxcontrols::c0068 {
-  file { '/etc/ssh/sshd_config':
-    ensure => present,
-    source => 'puppet:///modules/cis/el6/etc/ssh/sshd_config',
-    owner  => root,
-    group  => root,
-    mode   => '0600',
-  }
+#  file { '/etc/ssh/sshd_config':
+#    ensure => present,
+#    source => 'puppet:///modules/cis/el6/etc/ssh/sshd_config',
+#    owner  => root,
+#    group  => root,
+#    mode   => '0600',
+#  }
 }
