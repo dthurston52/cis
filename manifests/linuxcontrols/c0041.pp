7a564792d1efe05120390c4472c54d4e436b4336
U-NASHVILLE\dthurston <dthurston@dell821FXW1.Nashville.int>
2015-09-14 08:18:08 -0500

    Fix Firewall error conflicts c0041.pp

--- manifests/linuxcontrols/c0041.pp
+++ manifests/linuxcontrols/c0041.pp
@@ -6,33 +6,33 @@
 class cis::linuxcontrols::c0041 {
   case $::operatingsystem {
     'RedHat','Scientific','CentOS': {
-      case $::operatingsystemmajrelease {
-        6: {
-          package {'iptables':
-            ensure => installed,
-            before => Service['iptables'],
-          }
-          service {'iptables':
-            ensure     => running,
-            enable     => true,
-            hasrestart => true,
-            hasstatus  => true,
-          }
-        }
-        7: {
-          package {'firewalld':
-            ensure => installed,
-            before => Service['firewalld'],
-          }
-          service {'firewalld':
-            ensure     => running,
-            enable     => true,
-            hasrestart => true,
-            hasstatus  => true,
-          }
-        }
-        default: {}
-      }
+#      case $::operatingsystemmajrelease {
+#        6: {
+#          package {'iptables':
+#            ensure => installed,
+#            before => Service['iptables'],
+#          }
+#          service {'iptables':
+#            ensure     => running,
+#            enable     => true,
+#            hasrestart => true,
+#            hasstatus  => true,
+#          }
+#        }
+#        7: {
+#          package {'firewalld':
+#            ensure => installed,
+#            before => Service['firewalld'],
+#          }
+#          service {'firewalld':
+#            ensure     => running,
+#            enable     => true,
+#            hasrestart => true,
+#            hasstatus  => true,
+#          }
+#        }
+#        default: {}
+#      }
     }
     'Amazon': {
       package { 'iptables':
